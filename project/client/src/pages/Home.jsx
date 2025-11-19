46c69b9630cc498af8b442f4aab2fb664dcc7cda
Kelly Chan <152578750+kellykchan@users.noreply.github.com>
2025-11-19 12:48:48 +0000

    Add proxy: true to fix HTTPS redirect

--- project/client/src/pages/Home.jsx
+++ project/client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import "../css/Home.css";
 const Home = () => {
 
   const handleGoogleLogin = () => {
-    window.location.href = "https://stickerbackend.onrender.com/auth/google";
+    window.location.href = "https://stickerbackend.onrender.com/auth/google/callback";
   };
 
   return (
